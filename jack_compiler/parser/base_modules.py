4cad1ae1540b9c43b0cd9c215844b49d9248ca36
liuweilong <weilong.liu-csu@outlook.com>
2023-06-01 19:10:22 +0800

    继续重写逻辑handler

--- jack_compiler/parser/base_modules.py
+++ jack_compiler/parser/base_modules.py
@@ -43,6 +43,7 @@ class SupportHandler(BaseHandler):
 
     def processXML(self, word_and_type):
         self.xml = common_convert(word_and_type[1])(word_and_type[0])
+        return self.toXML()
 
 class NameHandler(BaseHandler):
     isTerminal = True
@@ -51,6 +52,7 @@ class NameHandler(BaseHandler):
     def processXML(self, unstructured_xml):
         if NameHandler.isTarget(unstructured_xml):
             self.xml += common_convert('identifier')(unstructured_xml[0][0])
+            return self.toXML()
         else:
             raise BaseException("something wrong with {0}".format(unstructured_xml))
     
@@ -72,55 +74,50 @@ class MultiUnitHandler(BaseHandler):
         super().__init__(unstructed_xml)
     
     def processXML(self, unstructured_xml):
-        if self.isMultiUnit(unstructured_xml):
+        if self.isTarget(unstructured_xml):
             find_length = 0
-            if self.base_unit:
-                find_length = self.findBaseUnit(unstructured_xml)
-                self.xml = self.base_unit.transform_function(unstructured_xml[:find_length])
-            for unit in self.option_units:
-                unit_length = unit.find_function(unstructured_xml[find_length:])
-                self.xml += unit.transform_function(unstructured_xml[find_length:find_length+unit_length])
+            if self.base_handler:
+                find_length = self.findBaseTarget(unstructured_xml)
+                self.xml = self.base_handler.processXML(unstructured_xml[:find_length])
+            for handler in self.options_handlers:
+                unit_length = handler.findTarget(unstructured_xml[find_length:])
+                self.xml += handler.processXML(unstructured_xml[find_length:find_length+unit_length])
                 find_length += unit_length
-            return self.xml
+            return self.toXML()
         else:
             raise BaseException("MultiUnitHandler: unstructured_xml is not a multi unit")
-
-    def isTarget(self,unstructured_xml):
-        if self.findMultiUnit(unstructured_xml) == len(unstructured_xml):
-            return True
-        return False
 
     # 使用 base_unit 来寻找开始的结果，如果找到了，就使用option_units来寻找后续的结果
     # 如果没有找到，就返回-1
     # 如果 base_unit == None , 就使用 option_units 来寻找结果
     # 如果 unstructured_xml == 0 , 就返回-1
     def findTarget(self,unstructured_xml):
-        if self.base_unit:
-            base_unit_length = self.findBaseUnit(unstructured_xml)
+        if self.base_handler:
+            base_unit_length = self.findBaseTarget(unstructured_xml)
             if base_unit_length < 0:
                 return -1
         else:
             base_unit_length = 0
-        option_units_length = self.findOptionUnits(unstructured_xml[base_unit_length:])
+        option_units_length = self.findOptionTarget(unstructured_xml[base_unit_length:])
         return base_unit_length if option_units_length < 0 else base_unit_length + option_units_length
 
-    def findBaseUnit(self,unstructured_xml):
+    def findBaseTarget(self,unstructured_xml):
         if not unstructured_xml:
             return -1
-        if self.base_unit:
-            return self.base_unit.find_function(unstructured_xml)
+        if self.base_handler:
+            return self.base_handler.findTarget(unstructured_xml)
         return 0
     
-    def findOptionUnits(self,unstructured_xml):
+    def findOptionTarget(self,unstructured_xml):
         if not unstructured_xml:
             return -1
         find_length = 0
-        for unit in self.option_units:
-            unit_length = unit.find_function(unstructured_xml)
+        for handler in self.options_handlers:
+            unit_length = handler.findTarget(unstructured_xml)
             if unit_length <0:
                 return -1
             find_length += unit_length
-        next_find_length = self.findOptionUnits(unstructured_xml[find_length:])
+        next_find_length = self.findOptionTarget(unstructured_xml[find_length:])
         return find_length if next_find_length < 0 else find_length + next_find_length
     
 class EmptyHandler(BaseHandler):
@@ -199,3 +196,54 @@ class OrHanlder(BaseHandler):
         if find_length2 < 0:
             return find_length1
 
+def check_chain_with_func_list(left_xml, func_list):
+    find_flag = [False] * len(func_list)
+    if not func_list:
+        raise BaseException('check_chain must have at least one function')
+    for index,func in enumerate(func_list):
+        find_length = func(left_xml)
+        if find_length >=0:
+            left_xml = left_xml[find_length:]
+            find_flag[index] = True
+        else:
+            break
+    return find_flag
+
+
+class SequenceHandler(BaseHandler):
+
+    check_chain = []
+    valid_num = []
+
+    def __init__(self, unstructed_xml=None):
+        super().__init__(unstructed_xml)
+
+    def processXML(self, unstructured_xml):
+        self.xml = ''
+        if self.isTarget(unstructured_xml):
+            find_flag = check_chain_with_func_list(unstructured_xml, [ find_function for _,find_function,_ in self.check_chain])
+            for index,function_pair in enumerate(self.check_chain):
+                item_name, find_function, process_function = function_pair
+                if find_flag[index]:
+                    find_length = find_function(unstructured_xml)
+                    self.xml += process_function(unstructured_xml[:find_length])
+                    unstructured_xml = unstructured_xml[find_length:]
+                else:
+                    break
+        else:
+            raise BaseException("SequenceHandler: unstructured_xml is not a unit")
+
+    def findTarget(self,unstructured_xml):
+        find_flag = check_chain_with_func_list(unstructured_xml, [ find_function for _,find_function,_ in self.check_chain])
+        # 如果 find_flag 中的 True 的个数不在 valid_num 中，返回 False
+        if find_flag.count(True) not in self.valid_num:
+            return -1
+        else:
+            find_length = 0
+            for index,function_pair in enumerate(self.check_chain):
+                item_name, find_function, process_function = function_pair
+                if find_flag[index]:
+                    find_length += find_function(unstructured_xml[find_length:])
+                else:
+                    break
+            return find_length   

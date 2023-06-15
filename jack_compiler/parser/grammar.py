26344f03f1a9f5baa940621eebbacb9338bd19eb
liuweilong <weilong.liu-csu@outlook.com>
2023-06-15 23:09:31 +0800

    解决之前没有看到的static 变量的问题

--- jack_compiler/parser/grammar.py
+++ jack_compiler/parser/grammar.py
@@ -24,6 +24,36 @@ class ProgramHandler(BaseHandler):
                 break
         return unstructured_xml
     
+    def registrateSymbolTable(self):
+        for child in self.children:
+            child.registrateSymbolTable()
+
+        # 检查是否存在重名的 class
+        class_names = [child.getClassName() for child in self.children]
+        if len(class_names) != len(set(class_names)):
+            raise GrammarException('find a class name repeated')
+        
+        # 获取所有child 的 symbol table
+        static_symbol_dict = {}
+        count = 0
+        for child in self.children:
+            for var_name, info_tuple in child.symbol_table:
+                child_class_name = child.getClassName()
+                static_name = '{0}.{1}'.format(child_class_name, var_name)
+                if static_name in static_symbol_dict:
+                    raise GrammarException('find a var_name{0} referred by multi_variables'.format(static_name))
+                static_symbol_dict[static_name] = copy.deepcopy(info_tuple)
+                static_symbol_dict[static_name][2] = count
+                count +=1 
+
+        # 将static_symbol_dict 注册到所有的 symbol_table 中
+        for child in self.children:
+            for var_name, info_tuple in static_symbol_dict.items():
+                child_class_name = child.getClassName()
+                static_name = '{0}.{1}'.format(child_class_name, var_name)
+                child.symbol_table[var_name] = static_symbol_dict[static_name]          
+        return
+
     def searchVariable(self, symbol):
         class_names = [child.getClassName() for child in self.children]
         # class_names 还需要添加一些BuiltIn Class
